f30b8d0fbacc381eb5a5bc74358d581d48c8fa06
paulocesar1701 <82987949+paulocesar1701@users.noreply.github.com>
2022-04-14 22:22:26 -0300

    projeto lanchonete

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SGLanchonete 1.0</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <h1>SGLanchonete 1.0</h1>
+    <a href="#" class="botao1">Cadastro de Produtos</a>
+    <a href="#" class="botao2">Cadastro de Pedidos</a>
+</body>
+</html>
